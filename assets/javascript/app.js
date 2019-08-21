ed82bf8010e4dc0346bd28e45a13bc9c32c8fc1f
colin <clawrence005@yahoo.com>
2019-08-21 18:13:16 -0500

    added wrong or right statements inbetween questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // intial values
 let counter = 5;
 let currentQuestion = 0;
-let score = 0;
+let wins = 0;
 let losses = 0;
 let timer;
 
@@ -30,7 +30,8 @@ function nextQuestion() {
 function timeIsUp() {
   clearInterval(timer);
   losses++;
-  nextQuestion();
+  preloadImage('lose');
+  setTimeout(nextQuestion, 3 * 1000);
 }
 
 function loadQuestion() {
@@ -42,6 +43,7 @@ function loadQuestion() {
   $('#time').html(`Timer: ${counter}`);
   $('#game').html(`<h4>  ${question}  </h4>
   ${loadChoices(choices)}
+  ${loadRemainingQuestions()}
   `)
 }
 
@@ -60,42 +62,72 @@ $(document).on('click', '.choice', function () {
   const selectedAnswer = $(this).attr('data-answer')
   console.log('clicked', selectedAnswer);
   const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
-  if (selectedAnswer === correctAnswer) {
+  //WIN
+  if (correctAnswer === selectedAnswer) {
     //score goes up
-    score++;
+    wins++;
+    preloadImage('win');
+    setTimeout(nextQuestion, 3 * 1000);
     console.log('you win');
-    nextQuestion();
+
+    //LOSE
   } else {
     losses++;
+    preloadImage('lose');
+    setTimeout(nextQuestion, 3 * 1000);
     console.log('you lose');
-    nextQuestion();
+
   }
 });
 
 function displayResult() {
   const result = `
- <h2>Game over</h2>
-  <p>You got ${score} /${quizQuestions.length} questions right</p>
+ <h2>Game over</h2> 
+  <p>You got ${wins / quizQuestions.length * 100}% of the questions right</p>
+  <p>You got ${wins} /${quizQuestions.length} questions right</p>
   <p>You missed ${losses}/${quizQuestions.length} questions</p>
-  <p>You got ${score / quizQuestions.length * 100}% of the questions right</p>
   <button class="btn btn-primary" id="reset">Reset Game</button>
   `;
   $('#game').html(result);
-  console.log(`score : ${score}, losses: ${losses} out of ${quizQuestions.length} questions `)
+  console.log(`score : ${wins}, losses: ${losses} out of ${quizQuestions.length} questions `)
 }
 // function reset() {
 $(document).on('click', '#reset', function () {
   console.log('reset clicked')
   counter = 5;
   currentQuestion = 0;
-  score = 0;
+  wins = 0;
   losses = 0;
   timer = null;
   loadQuestion();
 })
 // };
 
+function loadRemainingQuestions() {
+  const remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+  const totalQuestion = quizQuestions.length;
+  return `Remaining Question: ${remainingQuestion}/${totalQuestion}`
+}
 
+function preloadImage(status) {
+  const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+  if (status === 'win') {
+    $('#game').html(`
+<p class="preload-image">Congratulations, you picked the correct answer!</p>
+<p class="preload-image">The correct answer is <strong>${correctAnswer}</strong></p>
+<img src="">
+`);
+  } else {
+    $('#game').html(`
+    <p class="preload-image"> Nope You picked the wrong answer! </p>
+    <p class="preload-image">The correct answer is <strong>${correctAnswer}</strong></p>
+    `);
+  }
+}
 
-//start game
-loadQuestion();
+// intializes the game once button is pressed
+$('#start').click(function () {
+  $('#start').remove();
+  $('#timer').html(counter);
+  loadQuestion();
+});
