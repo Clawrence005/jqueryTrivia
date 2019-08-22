bd4925134d0fd6556dcf5bf7e859b09fddd1517f
colin <clawrence005@yahoo.com>
2019-08-22 13:29:47 -0500

    added gifs for right and wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // intial values
-let counter = 5;
+let counter = 15;
 let currentQuestion = 0;
 let wins = 0;
 let losses = 0;
@@ -30,12 +30,14 @@ function nextQuestion() {
 function timeIsUp() {
   clearInterval(timer);
   losses++;
-  preloadImage('lose');
+  $('#time').hide()
+  loadImage('lose');
   setTimeout(nextQuestion, 3 * 1000);
 }
 
 function loadQuestion() {
-  counter = 3;
+  $('#time').show()
+  counter = 15;
   timer = setInterval(countdown, 1000)
   const question = quizQuestions[currentQuestion].question;
   const choices = quizQuestions[currentQuestion].choices;
@@ -59,6 +61,7 @@ function loadChoices(choices) {
 // answers click handler
 $(document).on('click', '.choice', function () {
   clearInterval(timer);
+  $('#time').hide()
   const selectedAnswer = $(this).attr('data-answer')
   console.log('clicked', selectedAnswer);
   const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
@@ -66,14 +69,14 @@ $(document).on('click', '.choice', function () {
   if (correctAnswer === selectedAnswer) {
     //score goes up
     wins++;
-    preloadImage('win');
+    loadImage('win');
     setTimeout(nextQuestion, 3 * 1000);
     console.log('you win');
 
     //LOSE
   } else {
     losses++;
-    preloadImage('lose');
+    loadImage('lose');
     setTimeout(nextQuestion, 3 * 1000);
     console.log('you lose');
 
@@ -94,7 +97,7 @@ function displayResult() {
 // function reset() {
 $(document).on('click', '#reset', function () {
   console.log('reset clicked')
-  counter = 5;
+  counter = 15;
   currentQuestion = 0;
   wins = 0;
   losses = 0;
@@ -109,25 +112,58 @@ function loadRemainingQuestions() {
   return `Remaining Question: ${remainingQuestion}/${totalQuestion}`
 }
 
-function preloadImage(status) {
+const correctImageArray = [
+  './assets/gifs/right1.gif',
+  './assets/gifs/right2.gif',
+  './assets/gifs/right3.gif',
+  './assets/gifs/right4.gif',
+  './assets/gifs/right1.webp',
+  './assets/gifs/right2.webp',
+];
+
+const wrongImageArray = [
+  './assets/gifs/wrong1.gif',
+  './assets/gifs/wrong2.gif',
+  './assets/gifs/wrong3.gif',
+  './assets/gifs/wrong4.gif',
+  './assets/gifs/wrong5.gif',
+
+]
+
+function loadImage(status) {
   const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
   if (status === 'win') {
     $('#game').html(`
 <p class="preload-image">Congratulations, you picked the correct answer!</p>
+<img src="${showRandomImage(correctImageArray)}">
 <p class="preload-image">The correct answer is <strong>${correctAnswer}</strong></p>
-<img src="">
+
 `);
   } else {
     $('#game').html(`
     <p class="preload-image"> Nope You picked the wrong answer! </p>
+     <img src=${showRandomImage(wrongImageArray)}>
     <p class="preload-image">The correct answer is <strong>${correctAnswer}</strong></p>
+   
     `);
   }
 }
+//image arrays
 
+// creates a random image index number of which ever image array is passed in.
+function showRandomImage(imageArray) {
+  const randomIndex = Math.floor(Math.random() * imageArray.length);
+  const randomImage = imageArray[randomIndex];
+  return randomImage;
+}
 // intializes the game once button is pressed
 $('#start').click(function () {
   $('#start').remove();
   $('#timer').html(counter);
   loadQuestion();
 });
+
+// Tell Me About Yourself (This should be read as Why should I hire you? What do you bring to this position from your past experience and education?)
+
+
+// What are your career goals? (Think about this question as a way to tell an employer how you would perform as a developer and be realistic about where you are at in your career right now.)
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,21 +1,21 @@
 const quizQuestions = [
   {
-    question: "what does a chicken say _____ ",
+    question: "what does a chicken say? ",
     choices: ["cluck", "uck", "yuck", "ruff"],
     correctAnswer: "cluck",
   },
   {
-    question: "what does a dog say _____ ",
+    question: "what does a dog say?",
     choices: ["woof", "boof", "moof", "goof"],
     correctAnswer: "woof",
   },
   {
-    question: "what does a duck say _____ ",
+    question: "what does a duck say?",
     choices: ["quack", "squack", "tuack", "whack"],
     correctAnswer: "quack",
   },
   {
-    question: "what does a cow say _____ ",
+    question: "what does a cow say?",
     choices: ["moo", "boo", "loo", "goo"],
     correctAnswer: "moo",
   },
